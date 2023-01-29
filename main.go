e86515090c03303886af39ddd150b711c8e80df5
Igor Pomazkov <igor.pomazkov@betera.by>
2023-01-29 21:37:54 +0300

    removed http server

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"context"
-	"go.uber.org/fx"
-	"go.uber.org/zap"
-	"net/http"
-	"os"
+	"fmt"
+	"log"
+
+	"github.com/heetch/confita"
+	"github.com/heetch/confita/backend/env"
+	"github.com/igilgyrg/statbot/api/bots/telegram"
+	"github.com/igilgyrg/statbot/api/clients/coin"
+	"github.com/igilgyrg/statbot/internal/currency"
+	"github.com/igilgyrg/statbot/internal/storage/supabase"
+	"github.com/igilgyrg/statbot/schema"
 )
 
 type DBConfig struct {
@@ -16,90 +22,29 @@ type DBConfig struct {
 	DBName   string `config:"DB_NAME,required"`
 }
 
-func NewLogger() *zap.Logger {
-	logger, _ := zap.NewProduction()
-	return logger
-}
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-func NewMux(lc fx.Lifecycle, logger *zap.Logger) *http.ServeMux {
-	mux := http.NewServeMux()
-	server := &http.Server{
-		Addr:    ":80",
-		Handler: mux,
+	cfg := DBConfig{
+		Port: 5432,
 	}
 
-	lc.Append(fx.Hook{
-		OnStart: func(context.Context) error {
-			go server.ListenAndServe()
-			return nil
-		},
-		OnStop: func(ctx context.Context) error {
-			logger.Sync()
-			return server.Shutdown(ctx)
-		},
-	})
+	if err := confita.NewLoader(env.NewBackend()).Load(ctx, &cfg); err != nil {
+		log.Fatalf("sendsay: failed to load config: %s", err)
+	}
 
-	return mux
-}
+	url := fmt.Sprintf("postgres://%s:%s@%s:%d/%s", cfg.Username, cfg.Password, cfg.Host, cfg.Port, cfg.DBName)
+	schema.Migrate(&schema.DB, url)
+	builder := schema.New(ctx, url)
 
-func Register(mux *http.ServeMux, h http.Handler) {
-	mux.Handle("/5fb2054478353fd8d514056d1745b3a9eef066deadda4b90967af7ca65ce6505", h)
-}
+	client := coin.NewClient()
 
-var Module = fx.Provide(
-	NewHandler,
-)
+	repository := supabase.NewRepository(builder)
 
-var _commonComponents = fx.Options(
-	Module,
-)
+	service := currency.NewService(repository, client)
 
-func NewHandler() (http.Handler, error) {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		dat, err := os.ReadFile("./download.jpeg")
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
+	telegramClient := telegram.NewBot(service)
 
-			return
-		}
-
-		w.Write(dat)
-	}), nil
-}
-
-func main() {
-	//ctx, cancel := context.WithCancel(context.Background())
-	//defer cancel()
-
-	application := fx.New(
-		fx.Provide(
-			NewLogger,
-			NewMux,
-		),
-		_commonComponents,
-		fx.Invoke(Register),
-	)
-	application.Run()
-
-	//cfg := DBConfig{
-	//	Port: 5432,
-	//}
-	//
-	//if err := confita.NewLoader(env.NewBackend()).Load(ctx, &cfg); err != nil {
-	//	log.Fatalf("sendsay: failed to load config: %s", err)
-	//}
-	//
-	//url := fmt.Sprintf("postgres://%s:%s@%s:%d/%s", cfg.Username, cfg.Password, cfg.Host, cfg.Port, cfg.DBName)
-	//schema.Migrate(&schema.DB, url)
-	//builder := schema.New(ctx, url)
-	//
-	//client := coin.NewClient()
-	//
-	//repository := supabase.NewRepository(builder)
-	//
-	//service := currency.NewService(repository, client)
-	//
-	//telegramClient := telegram.NewBot(service)
-	//
-	//telegramClient.Run(ctx)
+	telegramClient.Run(ctx)
 }
